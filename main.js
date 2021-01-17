bd7ad2631645bf1146293ac78ca206ff11581e8b
sunkanmi <oguntimehins@gmail.com>
2021-01-17 23:01:57 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,79 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    
+    ]
+
+
+
+
+const wedDate = document.querySelector('.wed-date');
+const outcome = document.querySelector('h3');
+const countDown = document.querySelectorAll('.time p');
+
+
+const futureDate = new Date(2021, 1, 26, 10, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const date = futureDate.getDate()
+let month = futureDate.getMonth();
+month = months[month];
+let weekday = futureDate.getDay();
+weekday = weekdays[weekday];
+
+
+wedDate.innerHTML = `Date: ${weekday}  ${date}th ${month} ${year}, ${hours}:${mins}am`
+
+const futureTime = futureDate.getTime();
+
+
+const getCountDownStart = () =>{
+  const today = new Date().getTime();
+  const countDate = futureDate - today;
+  
+  // 1s = 1000
+  // 1m = 60s
+  // 1h = 60m
+  // 1d = 24
+  
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+  const oneSec = 1000;
+  
+  const days = Math.floor(countDate/oneDay);
+  const  hours = Math.floor((countDate % oneDay) / oneHour);
+  const mins = Math.floor((countDate % oneHour)/oneMin );
+  const secs = Math.floor((countDate % oneMin)/1000);
+  
+  
+  const value = [days, hours, mins, secs];
+  countDown.forEach((item, index) =>{
+    return item.innerHTML = value[index]
+  })
+  
+  
+}
+const counter = setInterval(getCountDownStart, 1000)
+getCountDownStart();
